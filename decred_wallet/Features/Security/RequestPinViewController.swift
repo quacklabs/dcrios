1953050f554bbe2c4c212793128662f8d5d859ac
Mark Boleigha <donhoenix@gmail.com>
2019-05-17 11:39:00 +0100

    made changes suggested
    Changes suggested on review of Pull request #2
    - Updated Cocoapods
    - fixed typos
    - removed unneccesary spaces

--- decred_wallet/Features/Security/RequestPinViewController.swift
+++ decred_wallet/Features/Security/RequestPinViewController.swift
@@ -61,7 +61,6 @@ class RequestPinViewController: SecurityBaseViewController {
     func pinUpdated(pin: String) {
         if self.requestPinConfirmation {
             let pinStrength = PinPasswordStrength.percentageStrength(of: pin)
-            
             self.prgsPinStrength.progressTintColor = pinStrength.color
             self.prgsPinStrength.progress = pinStrength.strength
         }
@@ -93,7 +92,7 @@ class RequestPinViewController: SecurityBaseViewController {
                 self.pinInputView.clear()
                 self.prgsPinStrength.progress = 0
                 
-                // We are starting again, show the strenght meter
+                // We're re-requesting input, show the strength meter
                 self.pinStrengthLabel.isHidden = false
                 self.prgsPinStrength.isHidden = false
             }
