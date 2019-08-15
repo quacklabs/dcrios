e08ca650f2f2066be505842a7f7dd5d85866220e
quacklabs <donhoenix@gmail.com>
2019-08-15 11:05:41 +0100

    create floating label class

---
+++ Decred Wallet/Custom Views/FloatingLabelTextField.swift
@@ -0,0 +1,14 @@
+//
+//  FloatingLabelTextField.swift
+//  Decred Wallet
+//
+//  Created by Sprinthub on 15/08/2019.
+//  Copyright © 2019 Decred. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+open class FloatingLabelTextField: UITextField{
+    
+}
